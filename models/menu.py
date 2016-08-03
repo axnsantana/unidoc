c9755a5e5750b6c4f91035241a00f9a9430fb1b7
Alex Santana <alex.santana@gmail.com>
2016-08-03 10:46:25 +0200

    Changing the menu organization.

--- models/menu.py
+++ models/menu.py
@@ -27,19 +27,23 @@ response.google_analytics_id = None
 
 response.menu = [
     (T('Home'), False, URL('default','index'), []),
-    (T("Patients"), False, URL('default','patients_list'), []),
-    (T('Groups'), False, URL('default','groups_list'), []),
-    (T('Experiments'), False, URL('default','experiments_list'), []),
-    (T('Researchers'), False, URL('default','researcher_list'), []),
-    (T('Exams'), False, URL('default','exams_list'), []),
+    (T("Dashboard"), False, '#', [
+        (T("Patients"), False, URL('dashboard','patients'), []),
+        (T("Experiments"), False, URL('dashboard','experiments'), []),
+    ]),
+    (T("Manage"), False, '#', [
+        (T("Patients"), False, URL('default','patients_list'), []),
+        (T('Groups'), False, URL('default','groups_list'), []),
+        (T('Experiments'), False, URL('default','experiments_list'), []),
+        (T('Researchers'), False, URL('default','researcher_list'), []),
+        (T('Exams'), False, URL('default','exams_list'), []),
+    ]),
     (T("Questionaries"), False, '#', [
-            (T("Statistics"), False, URL('quest','index')),
-            (T("Surveys"), False, URL('quest','surveys')),
-            (T("Files"), False, URL('quest','files'))
+        (T("Statistics"), False, URL('quest','index')),
+        (T("Surveys"), False, URL('quest','surveys')),
+        (T("Files"), False, URL('quest','files'))
     ]),
     (T("Administrative Interface"), False, URL('admin','default','index'), []),
-    #(T("Files"), False, URL('default','files'), []),
-    #(T('Database Map'), False, URL('default','map_files'), [])
     ]
 
 DEVELOPMENT_MENU = False
